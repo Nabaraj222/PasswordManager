144193c4abb59e5b1db9ba20936f2de25443e5f6
Nabaraj Ghimire <nabarajghimire222@gmail.com>
2021-11-10 19:37:01 +0545

    #1 various services are added.

--- App1/App1/App.xaml.cs
+++ App1/App1/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using App1.Services;
 using App1.Views;
 using System;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -8,7 +9,18 @@ namespace App1
 {
     public partial class App : Application
     {
-
+        static ProductService _productService;
+        public static ProductService ProductService
+        {
+            get
+            {
+                if (_productService == null)
+                {
+                    _productService = new ProductService(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),"Product.db3"));
+                }
+                return _productService; 
+            }
+        }
         public App()
         {
             InitializeComponent();
---
+++ App1/App1/Services/ProductService.cs
@@ -0,0 +1,47 @@
+﻿using App1.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1.Services
+{
+    public class ProductService : IProductRepository
+    {
+        public SQLiteAsyncConnection _database;
+        public ProductService(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<ProductInfo>().Wait();
+        }
+        public async Task<bool> AddProductAsync(ProductInfo product)
+        {
+            if (product.ProductId > 0)
+            {
+                await _database.UpdateAsync(product);
+            }
+            else
+            {
+                await _database.InsertAsync(product);
+            }
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> DeleteProductAsync(int id)
+        {
+            await _database.DeleteAsync<ProductInfo>(id);
+            return await Task.FromResult(true);
+        }
+
+        public async Task<ProductInfo> GetProductAsync(int id)
+        {
+            return await _database.Table<ProductInfo>().Where(P => P.ProductId == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<ProductInfo>> GetProductsAsync()
+        {
+            return await Task.FromResult(await _database.Table<ProductInfo>().ToListAsync());
+        }
+    }
+}
