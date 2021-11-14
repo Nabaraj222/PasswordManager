681146af2bf0c613a71c3a28983b05534afb342a
Nabaraj Ghimire <nabarajghimire222@gmail.com>
2021-11-14 22:55:31 +0545

    #1 finishes product display page

--- App1/App1/ViewModels/AddProductViewModel.cs
+++ App1/App1/ViewModels/AddProductViewModel.cs
@@ -2,11 +2,31 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace PasswordManager.ViewModels
 {
-    public class AddProductViewModel
+    public class AddProductViewModel:BaseProductViewModel
     {
+        public Command SaveCommand { get; }
+        public Command CancelCommand { get; }
+        public AddProductViewModel()
+        {
+            SaveCommand = new Command(OnSave);
+            CancelCommand = new Command(OnCancel);
+            this.PropertyChanged += (_, __) => SaveCommand.ChangeCanExecute();
+            ProductInfo = new Models.ProductInfo();
+        }
 
+        private async void OnSave()
+        {
+            var product = ProductInfo;
+            await App.ProductService.AddProductAsync(product);
+            await Shell.Current.GoToAsync("..");
+        }
+        private async void OnCancel()
+        {
+            await Shell.Current.GoToAsync("..");
+        }
     }
 }
--- App1/App1/Views/AddProductPage.xaml.cs
+++ App1/App1/Views/AddProductPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PasswordManager.Models;
+using PasswordManager.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,11 @@ namespace PasswordManager.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddProductPage : ContentPage
     {
+        public ProductInfo ProductInfo { get; set; }
         public AddProductPage()
         {
             InitializeComponent();
+            BindingContext = new AddProductViewModel();
         }
     }
 }
